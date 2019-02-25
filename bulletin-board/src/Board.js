d2c92c301a6cd8397f4f0ac70d4e8de4dc3055f1
trinayanbhatt <trinayanbhatt1@gmail.com>
2019-02-25 16:40:34 +0530

    Add new note feature integrated

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			notes: [
-				{
-					id: 0,
-					note: "Call Trinayan"
-				},
-				{
-					id: 1,
-					note: "Email Trinayan"
-				},
-				{
-					id: 2,
-					note: "Order food"
-				}
-			]
+			notes: []
 		}
+		this.add = this.add.bind(this)
 		this.eachNote = this.eachNote.bind(this)
 		this.update = this.update.bind(this)
 		this.remove = this.remove.bind(this)
+		this.nextId = this.nextId.bind(this)
+	}
+
+	add(text) {
+		this.setState(prevState => ({
+			notes: [
+				...prevState.notes,
+				{
+					id: this.nextId(),
+					note: text
+				}
+			]
+		}))
+	}
+
+	nextId() {
+		this.uniqueId = this.uniqueId || 0
+		return this.uniqueId++
 	}
 
 	update(newText, i) {
@@ -56,6 +63,10 @@ class Board extends Component {
 		return (
 			<div className="board">
 				{this.state.notes.map(this.eachNote)}
+				<button onClick={this.add.bind(null, "Let's add something to note")}
+						id="add">
+					<FaPlus />
+				</button>
 			</div>
 		)
 	}
