4fd5dda4b186770cb9c9c77aef67b24ac50a387b
trinayanbhatt <trinayanbhatt1@gmail.com>
2019-02-25 16:27:43 +0530

    board component added

--- bulletin-board/src/Note.js
+++ bulletin-board/src/Note.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			editing: false
+		}
 		this.edit = this.edit.bind(this)
 		this.remove = this.remove.bind(this)
+		this.save = this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
 	}
 	edit() {
-		alert('editing note')
+		this.setState({
+			editing: true
+		})
 	}
+
 	remove() {
 		alert('removing note')
 	}
-	render() {
+
+	save() {
+		alert(this._newText.value)
+	}
+
+	renderForm() {
 		return (
 			<div className="note">
-				<p>Learn React</p>
+				<form>
+					<textarea ref={input => this._newText = input}/>
+					<button onClick={this.save}><FaFloppyO /></button>
+				</form>
+			</div>
+		)
+	}
+
+	renderDisplay() {
+		return (
+			<div className="note">
+				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} id="edit"><FaPencil /></button>
 					<button onClick={this.remove} id="remove"><FaTrash /></button>
@@ -25,6 +51,15 @@ class Note extends Component {
 			</div>
 		)
 	}
+	render() {
+		return this.state.editing ? this.renderForm() : this.renderDisplay()
+	}
+
 }
 
 export default Note
+
+
+
+
+
